622e9f58a762dd1ee32b522f627a407eb54df46e
lexor351 <lexor351@yandex.ru>
2025-01-22 17:57:32 +0900

    Добавил выпадающий список при создании записи

--- src/resources/views/mainv2.blade.php
+++ src/resources/views/mainv2.blade.php
@@ -6,17 +6,49 @@
 
 <div class="form-container">
     <form method="POST" action="/main/check" class="form">
-       <div class="input-container"> <textarea type="email" name="email" id="email" placeholder="Введите срок годности карты"></textarea> </div>
-        <div class="input-container"> <input type="text" name="subject" id="subject" placeholder="Введите три цифры с оборота карты"></textarea> </div>
-         <div class="input-container"> <textarea name="message" id="message" class=form-control placeholder="Введите номер вашей карты"></textarea> </div>
-         <div class="select-container"> 
-            <h3>Выбрать</h3>
-            <select name="spec" id="spec">
-                <option value="skip">Пропустить</option>
-                <option value="BadLV">Бадюков</option>
-                <option value="Semenov">Семенов</option>
-                <option value="Teterin">Тетерин</option>
-            </select>
+        <div class="input-wrapper">
+            <div class="input-container"> <textarea name="message" id="message" class=form-control placeholder="Введите сообщение"></textarea> </div>
+            <div class="additional-fields">
+                <div class="select-container">
+                <h4>Клиент</h4>
+                    <select name="client" id="client">
+                        <option value="skip">Пропустить</option>
+                        <option value="gavs">ГАВС</option>
+                        <option value="asvs">АСВС</option>
+                        <option value="ugms">УГМС</option>
+                        <option value="young">Юные Якутяне</option>
+                        <option value="fkpas">ФКП АС</option>
+                        <option value="rusgidro">РусГидро</option>
+                        <option value="profavia">ПрофАвиа</option>
+                </select>
+                </div>
+                <div class="select-container">
+                    <h4>Тип услуги</h4>
+                    <select name="type" id="type">
+                        <option value="inet">Интернет</option>
+                        <option value="mach">Оборудование</option>
+                        <option value="sirena">Сирена</option>
+                        <option value="regina">Регина</option>
+                        <option value="endm">E&M канал</option>
+                        <option value="tele">Телефония</option>
+                        <option value="mail">Почта</option>
+                        <option value="website">Веб-сайты</option>
+                        <option value="vc">ВЦ</option>
+                        <option value="sat">Спутниковая связь</option>
+                        <option value="wifi">Беспроводная связь</option>
+                        <option value="vpn">ВПН</option>
+                    </select>
+                </div>
+                <div class="select-container"> 
+                    <h4>Выбрать ответственного</h4>
+                    <select name="spec" id="spec">
+                        <option value="skip">Пропустить</option>
+                        <option value="BadLV">Бадюков</option>
+                        <option value="Semenov">Семенов</option>
+                        <option value="Teterin">Тетерин</option>
+                    </select>
+                </div>
+            </div>
          </div>
      <button type="submit" class="button">Отправить</button>
     </form>
